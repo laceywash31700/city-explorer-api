8c6b13dc047683437885679f8bb7762f1da47a6d
Lacey Washington <lacey.m.washington@gmail.com>
2023-05-15 23:26:26 -0500

    completed lab 9

--- modules/getMovieData.js
+++ modules/getMovieData.js
@@ -6,9 +6,10 @@ class MovieData {
   constructor(obj) {
     this.title = obj.title;
     this.description = obj.overview;
-    this.avg_likes = obj.vote_average;
+    this.avg = obj.vote_average;
     this.total_likes = obj.vote_count;
     this.image = obj.poster_path;
+    this.backdrop = obj.backdrop_path;
     this.popularity = obj.popularity;
     this.released = obj.release_date;
   }
@@ -18,7 +19,7 @@ function getMovieData (req, res, next) {
   const {city} = req.query;
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API}&query=${city}&append_to_response=videos,images`;
   axios.get(url)
-    .then(res => res.data.results.map(n => new MovieData(n)).slice(0,4))
+    .then(res => res.data.results.filter(v => v.backdrop_path || v.poster_path ).map(n => new MovieData(n)).slice(0,4))
     .then(formattedData => res.status(200).send(formattedData))
     .catch(err => next(err));
 }
